8eb478bdf88587e6ae410efc2ed420e761c7a934
simatry <123036608+simatry@users.noreply.github.com>
2023-01-20 17:05:37 -0700

    Update data_loader.py
    Changes data source from tensorflow-datasets to huggingface's datasets module.

--- src/word2vec/data_loader.py
+++ src/word2vec/data_loader.py
@@ -1,20 +1,30 @@
 """ Creates TensorFlow datasets for model training."""
+import pathlib
+from typing import Union
 import tensorflow as tf
-import tensorflow_datasets as tfds
+import datasets
+
 from definitions import (
     VOCAB_SIZE,
     SEQUENCE_LENGTH,
-    DATA_DIR
+    DATA_DIR,
+    BATCH_SIZE
 )
 
 
-def get_tf_dataset(split: str=None):
-    data_dir = DATA_DIR / 'wiki40b'
-    dataset = tfds.load(
-        name='wiki40b/en',
+def get_tf_dataset(dataset_name: Union[str, pathlib.Path]='bookcorpus', split: str=None):
+    data_dir = DATA_DIR / dataset_name
+    dataset = datasets.load_dataset(
+        dataset_name,
+        cache_dir=data_dir,
         split=split,
-        data_dir=data_dir,
-        download=True
+        streaming=False
+    )
+    tf_dataset = dataset.to_tf_dataset(
+        columns=None,
+        shuffle=False,
+        batch_size=BATCH_SIZE,
+        collate_fn=None
     )
     tokenizer = tf.keras.preprocessing.text.Tokenizer(
         filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
@@ -25,15 +35,15 @@ def get_tf_dataset(split: str=None):
 
     return dataset
 
-def prepare_skipgram_batch():
+def collate_skipgram_batch():
     pass
 
-def prepare_cbow_batch():
+def collate_cbow_batch():
     pass
 
-
-dataset = get_tf_dataset(split='train')
-for thing in dataset:
-    print(thing)
+# Example using shakespeare since I don't have reviews handy.
+dataset = get_tf_dataset('bookcorpus', split='train')
+for batch in dataset:
+    print(batch)
 
     break
